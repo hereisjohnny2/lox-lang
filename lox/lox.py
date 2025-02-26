ef3b9b8b2539bdfa57a67e28964d5d181d401c60
joaoccardoso <joao.cardoso@fugro.com>
2025-02-26 15:48:01 -0300

    Create basic parser

--- lox/lox.py
+++ lox/lox.py
@@ -1,5 +1,7 @@
 import pprint
 
+from lox.ast_printer import AstPrinter
+from lox.parser import Parser
 from lox.scanner import Scanner
 
 
@@ -8,3 +10,12 @@ class Lox:
         scanner = Scanner(source)
         tokens = scanner.scan_tokens()
         pprint.pprint(tokens)
+
+        parser = Parser(tokens)
+        expr = parser.parse()
+
+        if not expr:
+            return
+
+        ast_printer = AstPrinter()
+        print(ast_printer.print(expr))
